d7c6c3ec0526ab1e0f41badb1fc4b2733de8d722
Tim Hyson <tjhyson@gmail.com>
2015-11-28 19:52:20 +0000

    refactors tests

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper.rb'
 
 feature 'Creating posts' do
-  scenario 'can create a post' do
+  background do
     visit '/'
     click_link 'New Post'
+  end
+  scenario 'can create a post' do
     attach_file('Image', "spec/files/images/coffee.jpg")
     fill_in 'Caption', with: 'nom nom nom #coffeetime'
     click_button 'Create Post'
@@ -12,8 +14,6 @@ feature 'Creating posts' do
   end
 
   it 'needs an image to create a post' do
-    visit '/'
-    click_link 'New Post'
     fill_in 'Caption', with: 'No picture because YOLO'
     click_button 'Create Post'
     expect(page).to have_content ('Halt, you fiend! You need an image to post here!')
