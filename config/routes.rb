73d6f4f26a64980f8bb80f85f3402899dc9b3c1e
Tim Hyson <tjhyson@gmail.com>
2015-11-30 16:39:30 +0000

    user can comment on posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
 end
